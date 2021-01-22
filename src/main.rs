4610f1e28bcb924e369e709cdb1c2e0b9ac1b3da
SAWADA Tadashi <cesare@mayverse.jp>
2021-01-22 23:06:25 +0900

    Implement minimal server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    });
+    server.bind("127.0.0.1:8000")?.run().await
 }
