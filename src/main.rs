4505747596e2d8086c3c8b8ee409998305bfb9d3
SAWADA Tadashi <cesare@mayverse.jp>
2021-01-24 14:48:26 +0900

    Enable to show sample

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Servant {
+    name: String,
+    class: String,
+}
+
+impl Servant {
+    fn new(name: impl Into<String>, class: impl Into<String>) -> Self {
+        Servant {
+            name: name.into(),
+            class: class.into(),
+        }
+    }
+}
+
+#[get["/servants/sample"]]
+async fn servants_sample() -> impl Responder {
+    HttpResponse::Ok().json(Servant::new("Meltryllis", "alterego"))
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -10,6 +31,7 @@ async fn main() -> std::io::Result<()> {
     let server = HttpServer::new(|| {
         App::new()
             .service(index)
+            .service(servants_sample)
     });
     server.bind("127.0.0.1:8000")?.run().await
 }
