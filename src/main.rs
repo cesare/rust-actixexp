a42173fd77d4a453442a6979f5f941acf56185e2
SAWADA Tadashi <cesare@mayverse.jp>
2021-01-26 21:59:56 +0900

    Implement listing endpoint naively

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
+use tokio_pg_mapper::FromTokioPostgresRow;
 use tokio_pg_mapper_derive::PostgresMapper;
+use tokio_postgres::{NoTls, Error};
 
 #[derive(Deserialize, PostgresMapper, Serialize)]
 #[pg_mapper(table = "servants")]
@@ -9,10 +11,31 @@ struct Servant {
     class: String,
 }
 
+async fn list_servants() -> Result<Vec<Servant>, Error> {
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres", NoTls).await?;
+
+    connection.await?;
+
+    let rows = client.query("select name, class from servants", &[]).await?;
+    let count = rows.len();
+    let mut results = Vec::with_capacity(count);
+    for row in rows {
+        let result = Servant::from_row(row).unwrap();
+        results.push(result);
+    }
+    Ok(results)
+}
+
 #[get["/servants"]]
 async fn servants() -> impl Responder {
-    let servants: Vec<Servant> = vec![];
-    HttpResponse::Ok().json(servants)
+    match list_servants().await {
+        Ok(servants) => HttpResponse::Ok().json(servants),
+        Err(_) => {
+            let empty: Vec<Servant> = vec![];
+            HttpResponse::Ok().json(empty)
+        }
+    }
 }
 
 #[get("/")]
