87a476dca588081de9148896d467802831757cf6
SAWADA Tadashi <cesare@mayverse.jp>
2021-01-30 21:49:26 +0900

    Enable to show single servant

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ struct Servant {
 
 #[derive(Error, Debug)]
 enum ActixexpError {
+    #[error("Not Found")]
+    NotFound,
     #[error("connection pool faild")]
     PoolFailed(#[from] deadpool_postgres::PoolError),
     #[error("query failed")]
@@ -26,7 +28,8 @@ enum ActixexpError {
 impl ResponseError for ActixexpError {
     fn error_response(&self) -> HttpResponse {
         match *self {
-            _ => HttpResponse::InternalServerError().finish()
+            ActixexpError::NotFound => HttpResponse::NotFound().finish(),
+            _ => HttpResponse::InternalServerError().finish(),
         }
     }
 }
@@ -52,11 +55,29 @@ async fn list_servants(client: &Client) -> Result<Vec<Servant>, ActixexpError> {
     Ok(results)
 }
 
+async fn show_servant(client: &Client, id: i32) -> Result<Servant, ActixexpError> {
+    let rows = client.query("select id, name, class from servants where id = $1", &[&id]).await?;
+    rows.iter()
+        .take(1)
+        .map(|row| Servant::from_row_ref(row).unwrap())
+        .collect::<Vec<Servant>>()
+        .pop()
+        .ok_or(ActixexpError::NotFound)
+}
+
 #[get["/servants"]]
 async fn servants(db_pool: web::Data<Pool>) -> Result<HttpResponse, ActixexpError> {
     let client = db_pool.get().await?;
     let results = list_servants(&client).await?;
     let response = HttpResponse::Ok().json(results);
+    Ok(response)
+}
+
+#[get("/servants/{id}")]
+async fn servant(db_pool: web::Data<Pool>, web::Path(id): web::Path<i32>) -> Result<HttpResponse, ActixexpError> {
+    let client = db_pool.get().await?;
+    let result = show_servant(&client, id).await?;
+    let response = HttpResponse::Ok().json(result);
     Ok(response)
 }
 
@@ -75,6 +96,7 @@ async fn main() -> std::io::Result<()> {
             .data(pool.clone())
             .service(index)
             .service(servants)
+            .service(servant)
     });
     server.bind("127.0.0.1:8000")?.run().await
 }
