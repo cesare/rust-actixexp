b65625ae60d33f94c2703967e22862ba400f5415
SAWADA Tadashi <cesare@mayverse.jp>
2021-02-06 22:58:33 +0900

    Refactor query method

--- src/app/db.rs
+++ src/app/db.rs
@@ -1,6 +1,8 @@
 use deadpool_postgres::Client;
 use serde::Deserialize;
 use tokio_pg_mapper::FromTokioPostgresRow;
+use tokio_postgres::ToStatement;
+use tokio_postgres::types::ToSql;
 
 use crate::app::Result;
 use crate::app::errors::ActixexpError;
@@ -24,40 +26,32 @@ impl ServantRepository {
     }
 
     pub async fn create(&self, request: CreateServantRequest) -> Result<Servant> {
-        let rows = self.client.query("insert into servants (name, class) values ($1, $2) returning id, name, class", &[&request.name, &request.class]).await?;
-        rows.iter()
-            .take(1)
-            .map(|row| Servant::from_row_ref(row).unwrap())
-            .collect::<Vec<Servant>>()
+        self.query("insert into servants (name, class) values ($1, $2) returning id, name, class", &[&request.name, &request.class]).await?
             .pop()
             .ok_or(ActixexpError::NotFound)
     }
 
     pub async fn list(&self) -> Result<Vec<Servant>> {
-        let rows = self.client.query("select id, name, class from servants", &[]).await?;
-        let results = rows.iter()
-            .map(|row| Servant::from_row_ref(row).unwrap())
-            .collect();
-        Ok(results)
+        self.query("select id, name, class from servants", &[]).await
     }
 
     pub async fn show(&self, id: i32) -> Result<Servant> {
-        let rows = self.client.query("select id, name, class from servants where id = $1", &[&id]).await?;
-        rows.iter()
-            .take(1)
-            .map(|row| Servant::from_row_ref(row).unwrap())
-            .collect::<Vec<Servant>>()
+        self.query("select id, name, class from servants where id = $1", &[&id]).await?
             .pop()
             .ok_or(ActixexpError::NotFound)
     }
 
     pub async fn delete(&self, id: i32) -> Result<Servant> {
-        let rows = self.client.query("delete from servants where id = $1 returning id, name, class", &[&id]).await?;
-        rows.iter()
-            .take(1)
-            .map(|row| Servant::from_row_ref(row).unwrap())
-            .collect::<Vec<Servant>>()
+        self.query("delete from servants where id = $1 returning id, name, class", &[&id]).await?
             .pop()
             .ok_or(ActixexpError::NotFound)
+    }
+
+    async fn query<T: ?Sized + ToStatement>(&'_ self, statement: &'_ T, params: &'_ [&'_ (dyn ToSql + Sync)]) -> Result<Vec<Servant>> {
+        let rows = self.client.query(statement, params).await?;
+        let servants = rows.iter()
+            .map(|row| Servant::from_row_ref(row).unwrap())
+            .collect::<Vec<Servant>>();
+        Ok(servants)
     }
 }
