4be7a545e6cf32633d9ec0905f18143d16fef848
SAWADA Tadashi <cesare@mayverse.jp>
2021-09-09 21:41:27 +0900

    Enable to configure token signing key

--- src/app/config/auth.rs
+++ src/app/config/auth.rs
@@ -1,7 +1,21 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
+use serde::de::Error;
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct AuthConfig {
     pub client_id: String,
     pub client_secret: String,
+
+    #[serde(deserialize_with="deserialize_base64")]
+    pub token_signing_key: Vec<u8>,
+}
+
+fn deserialize_base64<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+    where D: Deserializer<'de>
+{
+    String::deserialize(deserializer)
+        .and_then(|s|
+            base64::decode(&s)
+                .map_err(|err| Error::custom(err.to_string())
+        ))
 }
