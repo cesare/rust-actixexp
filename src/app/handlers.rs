6d9ed9bb9eca4459a6cb2f815a8efa36ba40329f
SAWADA Tadashi <cesare@mayverse.jp>
2021-11-11 21:19:52 +0900

    Enable to respond errors with JSON message

--- src/app/handlers.rs
+++ src/app/handlers.rs
@@ -1,4 +1,5 @@
 use actix_web::{HttpResponse, ResponseError};
+use serde_json::json;
 
 use super::db::DatabaseError;
 
@@ -9,8 +10,22 @@ pub mod servant;
 impl ResponseError for DatabaseError {
     fn error_response(&self) -> HttpResponse {
         match *self {
-            Self::NotFound => HttpResponse::NotFound().finish(),
-            _ => HttpResponse::InternalServerError().finish(),
+            Self::NotFound => generic_not_found_response(),
+            _ => generic_internal_server_error_response(),
         }
     }
+}
+
+fn generic_not_found_response() -> HttpResponse {
+    let body = json!({
+        "error": "not found",
+    });
+    HttpResponse::NotFound().json(body)
+}
+
+fn generic_internal_server_error_response() -> HttpResponse {
+    let body = json!({
+        "error": "internal server error",
+    });
+    HttpResponse::InternalServerError().json(body)
 }
