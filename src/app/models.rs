5a3566337362e4efafe01309f2a6060f5e8e7389
SAWADA Tadashi <cesare@mayverse.jp>
2021-11-18 22:31:51 +0900

    Add domain error

--- src/app/models.rs
+++ src/app/models.rs
@@ -1,6 +1,31 @@
+use thiserror::Error;
+
 pub mod auth;
 pub mod identity;
 pub mod servant;
 
 pub use identity::Identity;
 pub use servant::Servant;
+
+use super::db::DatabaseError;
+
+#[derive(Error, Debug)]
+pub enum DomainError {
+    #[error("Requested record is not found")]
+    RecordNotFound,
+
+    #[error("Database error: {source}")]
+    DatabaseError {
+        #[source]
+        source: DatabaseError,
+    }
+}
+
+impl From<DatabaseError> for DomainError {
+    fn from(e: DatabaseError) -> Self {
+        match e {
+            DatabaseError::NotFound => Self::RecordNotFound,
+            _ => Self::DatabaseError { source: e },
+        }
+    }
+}
