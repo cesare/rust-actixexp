209b388ad5f759206fed964c2769966861afab27
SAWADA Tadashi <cesare@mayverse.jp>
2021-12-05 20:20:49 +0900

    Fix indentation

--- src/app/models/servant.rs
+++ src/app/models/servant.rs
@@ -9,158 +9,158 @@ use super::DomainError;
 #[derive(Deserialize, PostgresMapper, Serialize)]
 #[pg_mapper(table = "servants")]
 pub struct Servant {
-  id: i32,
-  name: String,
-  class_name: String,
+    id: i32,
+    name: String,
+    class_name: String,
 }
 
 pub enum ServantClass {
-  Saber,
-  Archer,
-  Lancer,
-  Rider,
-  Caster,
-  Assassin,
-  Berserker,
-  Ruler,
-  Avenger,
-  Mooncancer,
-  Alterego,
-  Foreigner,
-  Pretender,
-  Shielder,
+    Saber,
+    Archer,
+    Lancer,
+    Rider,
+    Caster,
+    Assassin,
+    Berserker,
+    Ruler,
+    Avenger,
+    Mooncancer,
+    Alterego,
+    Foreigner,
+    Pretender,
+    Shielder,
 }
 
 impl From<ServantClass> for String {
-  fn from(clazz: ServantClass) -> String {
-    let string_representation = match clazz {
-      ServantClass::Saber      => "saber",
-      ServantClass::Archer     => "archer",
-      ServantClass::Lancer     => "lancer",
-      ServantClass::Rider      => "rider",
-      ServantClass::Caster     => "caster",
-      ServantClass::Assassin   => "assassin",
-      ServantClass::Berserker  => "berserker",
-      ServantClass::Ruler      => "ruler",
-      ServantClass::Avenger    => "avenger",
-      ServantClass::Mooncancer => "mooncancer",
-      ServantClass::Alterego   => "alterego",
-      ServantClass::Foreigner  => "foreigner",
-      ServantClass::Pretender  => "pretender",
-      ServantClass::Shielder   => "shielder",
-    };
-    string_representation.to_owned()
-  }
+    fn from(clazz: ServantClass) -> String {
+        let string_representation = match clazz {
+            ServantClass::Saber      => "saber",
+            ServantClass::Archer     => "archer",
+            ServantClass::Lancer     => "lancer",
+            ServantClass::Rider      => "rider",
+            ServantClass::Caster     => "caster",
+            ServantClass::Assassin   => "assassin",
+            ServantClass::Berserker  => "berserker",
+            ServantClass::Ruler      => "ruler",
+            ServantClass::Avenger    => "avenger",
+            ServantClass::Mooncancer => "mooncancer",
+            ServantClass::Alterego   => "alterego",
+            ServantClass::Foreigner  => "foreigner",
+            ServantClass::Pretender  => "pretender",
+            ServantClass::Shielder   => "shielder",
+        };
+        string_representation.to_owned()
+    }
 }
 
 impl TryFrom<&str> for ServantClass {
-  type Error = ServantClassConversionError;
+    type Error = ServantClassConversionError;
 
-  fn try_from(value: &str) -> Result<Self, Self::Error> {
-    match value {
-      "saber"      => Ok(Self::Saber),
-      "archer"     => Ok(Self::Archer),
-      "lancer"     => Ok(Self::Lancer),
-      "rider"      => Ok(Self::Rider),
-      "caster"     => Ok(Self::Caster),
-      "assassin"   => Ok(Self::Assassin),
-      "berserker"  => Ok(Self::Berserker),
-      "ruler"      => Ok(Self::Ruler),
-      "avenger"    => Ok(Self::Avenger),
-      "mooncancer" => Ok(Self::Mooncancer),
-      "alterego"   => Ok(Self::Alterego),
-      "foreigner"  => Ok(Self::Foreigner),
-      "pretender"  => Ok(Self::Pretender),
-      "shielder"   => Ok(Self::Shielder),
-      _ => Err(ServantClassConversionError::UnknownClass)
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "saber"      => Ok(Self::Saber),
+            "archer"     => Ok(Self::Archer),
+            "lancer"     => Ok(Self::Lancer),
+            "rider"      => Ok(Self::Rider),
+            "caster"     => Ok(Self::Caster),
+            "assassin"   => Ok(Self::Assassin),
+            "berserker"  => Ok(Self::Berserker),
+            "ruler"      => Ok(Self::Ruler),
+            "avenger"    => Ok(Self::Avenger),
+            "mooncancer" => Ok(Self::Mooncancer),
+            "alterego"   => Ok(Self::Alterego),
+            "foreigner"  => Ok(Self::Foreigner),
+            "pretender"  => Ok(Self::Pretender),
+            "shielder"   => Ok(Self::Shielder),
+            _ => Err(ServantClassConversionError::UnknownClass)
+        }
     }
-  }
 }
 
 pub enum ServantClassConversionError {
-  UnknownClass,
+    UnknownClass,
 }
 
 pub struct ServantRegistration<'a> {
-  context: &'a Context,
-  name: String,
-  class_name: String,
+    context: &'a Context,
+    name: String,
+    class_name: String,
 }
 
 impl<'a> ServantRegistration<'a> {
-  pub fn new(context: &'a Context, name: &str, class_name: &str) -> Self {
-    Self {
-      context: context,
-      name: name.to_owned(),
-      class_name: class_name.to_owned(),
+    pub fn new(context: &'a Context, name: &str, class_name: &str) -> Self {
+      Self {
+          context: context,
+          name: name.to_owned(),
+          class_name: class_name.to_owned(),
+      }
     }
-  }
 
-  pub async fn execute(&self) -> Result<Servant, DomainError> {
-    let repository = ServantRepository::initialize(&self.context.db).await?;
-    let dataset = RegistrationDataset {
-      name: self.name.to_owned(),
-      class_name: self.class_name.to_owned(),
-    };
-    let servant = repository.create(dataset).await?;
-    Ok(servant)
-  }
+    pub async fn execute(&self) -> Result<Servant, DomainError> {
+        let repository = ServantRepository::initialize(&self.context.db).await?;
+        let dataset = RegistrationDataset {
+            name: self.name.to_owned(),
+            class_name: self.class_name.to_owned(),
+        };
+        let servant = repository.create(dataset).await?;
+        Ok(servant)
+    }
 }
 
 pub struct ServantListing<'a> {
-  context: &'a Context,
+    context: &'a Context,
 }
 
 impl<'a> ServantListing<'a> {
-  pub fn new(context: &'a Context) -> Self {
-    Self {
-      context: context,
+    pub fn new(context: &'a Context) -> Self {
+        Self {
+            context: context,
+        }
     }
-  }
 
-  pub async fn execute(&self) -> Result<Vec<Servant>, DomainError> {
-    let repository = ServantRepository::initialize(&self.context.db).await?;
-    let servants = repository.list().await?;
-    Ok(servants)
-  }
+    pub async fn execute(&self) -> Result<Vec<Servant>, DomainError> {
+        let repository = ServantRepository::initialize(&self.context.db).await?;
+        let servants = repository.list().await?;
+        Ok(servants)
+    }
 }
 
 pub struct ServantFetching<'a> {
-  context: &'a Context,
-  id: i32,
+    context: &'a Context,
+    id: i32,
 }
 
 impl<'a> ServantFetching<'a> {
-  pub fn new(context: &'a Context, id: i32) -> Self {
-    Self {
-      context: context,
-      id: id,
+    pub fn new(context: &'a Context, id: i32) -> Self {
+        Self {
+            context: context,
+            id: id,
+        }
     }
-  }
 
-  pub async fn execute(&self) -> Result<Servant, DomainError> {
-    let repository = ServantRepository::initialize(&self.context.db).await?;
-    let servants = repository.show(self.id).await?;
-    Ok(servants)
-  }
+    pub async fn execute(&self) -> Result<Servant, DomainError> {
+        let repository = ServantRepository::initialize(&self.context.db).await?;
+        let servants = repository.show(self.id).await?;
+        Ok(servants)
+    }
 }
 
 pub struct ServantDeletion<'a> {
-  context: &'a Context,
-  id: i32,
+    context: &'a Context,
+    id: i32,
 }
 
 impl<'a> ServantDeletion<'a> {
-  pub fn new(context: &'a Context, id: i32) -> Self {
-    Self {
-      context: context,
-      id: id,
+    pub fn new(context: &'a Context, id: i32) -> Self {
+        Self {
+            context: context,
+            id: id,
+        }
     }
-  }
 
-  pub async fn execute(&self) -> Result<Servant, DomainError> {
-    let repository = ServantRepository::initialize(&self.context.db).await?;
-    let servant = repository.delete(self.id).await?;
-    Ok(servant)
-  }
+    pub async fn execute(&self) -> Result<Servant, DomainError> {
+        let repository = ServantRepository::initialize(&self.context.db).await?;
+        let servant = repository.delete(self.id).await?;
+        Ok(servant)
+    }
 }
